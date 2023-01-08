be78d933bf5a729021078182409a5ed961e05a7a
yan-3121 <ty.ty.ty.ty.0004@gmail.com>
2023-01-08 17:49:33 +0900

    add:hamburger-menu
    fix

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,19 @@
 const ham = $('#js-hamburger');
 const nav = $('#js-nav');
+const future = $('#futureLink');
+const contact = $('#contactLink')
+
 ham.on('click' , function () {
     ham.toggleClass('active');
     nav.toggleClass('active');
+});
+
+future.on('click' , function() {
+    ham.removeClass('active');
+    nav.removeClass('active');
+});
+
+contact.on('click' , function() {
+    ham.removeClass('active');
+    nav.removeClass('active');
 });
