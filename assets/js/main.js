991a32a049ddd27c75d83842502183b8ff0e7ea7
yan-3121 <ty.ty.ty.ty.0004@gmail.com>
2023-01-20 21:13:59 +0900

    add:slick
    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,55 @@
-const ham = $('#js-hamburger');
-const nav = $('#js-nav');
-const future = $('#futureLink');
-const contact = $('#contactLink');
-const body = $('body');
+$(function(){
+    const ham = $('#js-hamburger');
+    const nav = $('#js-nav');
+    const future = $('#futureLink');
+    const contact = $('#contactLink');
+    const body = $('body');
+    
+    ham.on('click' , function () {
+        ham.toggleClass('active');
+        nav.toggleClass('active');
+    });
+    
+    future.on('click' , function() {
+        ham.removeClass('active');
+        nav.removeClass('active');
+    });
+    
+    contact.on('click' , function() {
+        ham.removeClass('active');
+        nav.removeClass('active');
+    });
+    
+    body.on('click' , function() {
+        body.toggleClass('active')
+    });
+    
+    $('.slick')
+    .on('init', function () {
+        $('.slick-slide[data-slick-index="0"]').addClass('add-animation');
+    })
 
-ham.on('click' , function () {
-    ham.toggleClass('active');
-    nav.toggleClass('active');
+    $('.slick01').slick({
+        autoplay: true,
+        fade: true,
+        speed: 2000,
+        autoplaySpeed: 4000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        dots: false,
+        arrows: false,
+    }
+    )  
+
+    .on({
+        beforeChange: function (event, slick, currentSlide, nextSlide) {
+          $(".slick-slide", this).eq(nextSlide).addClass("add-animation");
+          $(".slick-slide", this).eq(currentSlide).addClass("remove-animation");
+        },
+        afterChange: function () {
+          $(".remove-animation", this).removeClass(
+            "remove-animation add-animation"
+          );
+        },
+      });
 });
-
-future.on('click' , function() {
-    ham.removeClass('active');
-    nav.removeClass('active');
-});
-
-contact.on('click' , function() {
-    ham.removeClass('active');
-    nav.removeClass('active');
-});
-
-body.on('click' , function() {
-    body.toggleClass('active')
-});
-
-$('.slick01').slick({
-    autoplay: true,
-    dots: false,
-    arrows: false,
-}
-);
