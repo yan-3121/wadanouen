8b085253aa22a69016d6231a702e6d0a9e76d557
yan-3121 <ty.ty.ty.ty.0004@gmail.com>
2023-01-25 12:47:59 +0900

    add:future-modal
    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,4 +52,16 @@ $(function(){
           );
         },
       });
+      $(function () {
+        $('.button').prevAll().hide();
+        $('.button').click(function () {
+            if ($(this).prevAll().is(':hidden')) {
+                $(this).prevAll().slideDown();
+                $(this).text('close').addClass('close');
+            } else {
+                $(this).prevAll().slideUp();
+                $(this).text('資料▼').removeClass('close');
+              }
+          });
+      });
 });
