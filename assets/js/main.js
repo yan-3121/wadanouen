f43d8528d74faac3e690034b1fedfb5f370479a8
yan-3121 <ty.ty.ty.ty.0004@gmail.com>
2023-11-17 16:51:21 +0900

    [add]20231117

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,5 +64,35 @@ $(function(){
           });
         });
       });
+
+        // ローダー終了
+        function end_loader() {
+          $('.loader').fadeOut(800);
+        }
+        // テキスト表示
+        function show_txt() {
+          $('.loader .loader__logo').fadeIn(400);
+        }
+        // テキスト非表示
+        function hide_txt() {
+          $('.loader .loader__logo').fadeOut(400);
+        }
+
+        // タイマー処理
+        $(window).on('load', function () {
+          // 処理①
+          setTimeout(function () {
+            show_txt();
+          }, 500)
+          // 処理②
+          setTimeout(function () {
+            hide_txt();
+          }, 3500)
+          // 処理③
+          setTimeout(function () {
+            end_loader();
+          }, 4500)
+        })
+
 });
 
